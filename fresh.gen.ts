b80426b8bdb20d17149195462dc4930be042d22d
hermesonbastos <hermesonbastos@gmail.com>
2024-03-17 20:31:02 -0300

    feat: dynamic routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,6 +4,7 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $about from "./routes/about.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $hello from "./routes/hello.tsx";
@@ -16,6 +17,7 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/about.tsx": $about,
     "./routes/api/joke.ts": $api_joke,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/hello.tsx": $hello,
--- routes/greet/[name].tsx
+++ routes/greet/[name].tsx
@@ -1,5 +1,12 @@
 import { PageProps } from "$fresh/server.ts";
 
-export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+export default function GreetPage(props: PageProps) {
+
+  const { name } = props.params;
+
+  return (
+    <main>
+      <h1>Geetings to you, {name}</h1>
+    </main>
+  )
 }
