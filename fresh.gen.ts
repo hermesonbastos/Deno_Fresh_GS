f0d63635cf73d655217c19d3c142195c4d1b509b
hermesonbastos <hermesonbastos@gmail.com>
2024-03-17 20:16:52 -0300

    feat: new route oomponent

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,8 +6,10 @@ import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
+import * as $hello from "./routes/hello.tsx";
 import * as $index from "./routes/index.tsx";
 import * as $Counter from "./islands/Counter.tsx";
+import * as $HelloWorld from "./islands/HelloWorld.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
@@ -16,10 +18,12 @@ const manifest = {
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
     "./routes/greet/[name].tsx": $greet_name_,
+    "./routes/hello.tsx": $hello,
     "./routes/index.tsx": $index,
   },
   islands: {
     "./islands/Counter.tsx": $Counter,
+    "./islands/HelloWorld.tsx": $HelloWorld,
   },
   baseUrl: import.meta.url,
 } satisfies Manifest;
---
+++ routes/hello.tsx
@@ -0,0 +1,7 @@
+import HelloWorld from '../islands/HelloWorld.tsx'
+
+export default function Hello() {
+  return <div>
+    <HelloWorld />
+  </div>
+}
